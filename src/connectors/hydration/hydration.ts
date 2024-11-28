b41a0f6c459d704fb37b71761463c9a89adbe3ca
Gregory benneth <gregory.benneth@funttastic.com>
2024-11-28 15:55:54 -0300

    feat: add slippage calculation to Hydration connector

--- src/connectors/hydration/hydration.ts
+++ src/connectors/hydration/hydration.ts
@@ -3,6 +3,7 @@ import { Polkadot } from '../../chains/polkadot/polkadot';
 import { HydrationConfig } from './hydration.config';
 import { PriceRequest, TradeRequest } from '../../amm/amm.requests';
 import { getPolkadotConfig } from '../../chains/polkadot/polkadot.config';
+import { percentRegexp } from '../../services/config-manager-v2';
 
 export class Hydration {
   private static _instances: LRUCache<string, Hydration>;
@@ -11,8 +12,8 @@ export class Hydration {
   private _ready: boolean = false;
 
   constructor(network: string) {
-    this.chain = Polkadot.getInstance(network);
     this._config = HydrationConfig.config;
+    this.chain = Polkadot.getInstance(network);
   }
 
   public static getInstance(network: string): Hydration {
@@ -22,7 +23,7 @@ export class Hydration {
         max: config.network.maxLRUCacheInstances,
       });
     }
-    
+
     if (!Hydration._instances) {
       Hydration._instances = new LRUCache<string, Hydration>({ max: 10 });
     }
@@ -46,7 +47,13 @@ export class Hydration {
   public ready(): boolean {
     return this._ready;
   }
-
+  getSlippage(): number {
+    const allowedSlippage = this._config.allowedSlippage;
+    const nd = allowedSlippage.match(percentRegexp);
+    let slippage = 0.0;
+    if (nd) slippage = Number(nd[1]) / Number(nd[2]);
+    return slippage;
+  }
   public async estimateTrade(req: PriceRequest) {
     const price = await this.chain.getBalance(req.base);
     const amount = req.side === 'BUY' ? Number(req.amount) / Number(price) : Number(req.amount) * Number(price);
